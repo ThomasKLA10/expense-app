e1455bf8598627812440258bf9450b9c7dd081c0
Thomas <150798898+ThomasKLA10@users.noreply.github.com>
2024-12-16 17:26:59 +0100

    added upload form and route

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required
+from werkzeug.utils import secure_filename
+import os
+from . import db
+from .models import Receipt
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # We'll implement this later
+        return redirect(url_for('main.index'))
+    return render_template('upload.html')
